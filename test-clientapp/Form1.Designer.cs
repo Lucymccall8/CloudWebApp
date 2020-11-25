29dcf1328db810aa3850644817f2ec699aa9b5ab
Lucymccall8 <56158214+Lucymccall8@users.noreply.github.com>
2020-11-25 17:46:51 +0000

    Uploaded WinForms Client

---
+++ test-clientapp/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace test_clientapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btGenerate = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbResultDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BMI Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Weight:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender:";
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(83, 62);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(100, 23);
+            this.tbHeight.TabIndex = 4;
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(83, 94);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(100, 23);
+            this.tbWeight.TabIndex = 5;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(107, 128);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(51, 19);
+            this.rbMale.TabIndex = 6;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(107, 153);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(63, 19);
+            this.rbFemale.TabIndex = 7;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "cm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(193, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "kg";
+            // 
+            // btGenerate
+            // 
+            this.btGenerate.Location = new System.Drawing.Point(39, 178);
+            this.btGenerate.Name = "btGenerate";
+            this.btGenerate.Size = new System.Drawing.Size(174, 23);
+            this.btGenerate.TabIndex = 10;
+            this.btGenerate.Text = "Generate";
+            this.btGenerate.UseVisualStyleBackColor = true;
+            this.btGenerate.Click += new System.EventHandler(this.btGenerate_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(265, -66);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 30);
+            this.label7.TabIndex = 11;
+            // 
+            // lbResultDescription
+            // 
+            this.lbResultDescription.AutoSize = true;
+            this.lbResultDescription.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbResultDescription.Location = new System.Drawing.Point(21, 242);
+            this.lbResultDescription.MaximumSize = new System.Drawing.Size(220, 300);
+            this.lbResultDescription.MinimumSize = new System.Drawing.Size(220, 30);
+            this.lbResultDescription.Name = "lbResultDescription";
+            this.lbResultDescription.Size = new System.Drawing.Size(220, 30);
+            this.lbResultDescription.TabIndex = 12;
+            this.lbResultDescription.Text = " ";
+            this.lbResultDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 329);
+            this.Controls.Add(this.lbResultDescription);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btGenerate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btGenerate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbResultDescription;
+    }
+}
+
---
+++ test-clientapp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceReference3;
+
+namespace test_clientapp
+{
+    public partial class Form1 : Form
+    {
+        //create BMI int for holding result
+        int BMI;
+        string result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btGenerate_Click(object sender, EventArgs e)
+        {
+            //create a new client
+            BMICalcClient client = new BMICalcClient();
+            //set height and weight as doubles for calculatebmi method
+            double Height = double.Parse(tbHeight.Text);
+            double Weight = double.Parse(tbWeight.Text);
+           
+
+            //if user is a female, call calculatebmi method as such
+            if (rbFemale.Checked == true)
+            {
+                lbResultDescription.Text = client.BMIResultAsync(Height, Weight, "Female").Result;
+            } else
+            {
+               lbResultDescription.Text = client.BMIResultAsync(Height, Weight, "Male").Result;
+            }
+
+            
+            client.CloseAsync();
+        }
+
+    }
+}
