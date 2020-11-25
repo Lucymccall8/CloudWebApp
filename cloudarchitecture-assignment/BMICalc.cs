827bd4af0b18ac52f50841e860c84e9244330df5
Lucymccall8 <56158214+Lucymccall8@users.noreply.github.com>
2020-11-25 17:45:20 +0000

    Uploaded WCF App

---
+++ cloudarchitecture-assignment/BMICalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace cloudarchitecture_assignment
+{
+    [ServiceContract]
+    public interface BMICalc
+    {
+        //Defining the methods that can be used within the main code.
+        [OperationContract]
+        string BMIResult(double height, double weight, string gender);
+
+    }
+
+}
---
+++ cloudarchitecture-assignment/BMICalc.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace cloudarchitecture_assignment
+{
+    public class Service1 : BMICalc
+    {
+
+        public string BMIResult(double height, double weight, string gender)
+        {
+            double BMI = weight / Math.Pow(height / 100.0, 2); //calculate the BMI and store as double
+
+            if (gender == "female") //if user is female, display result based on these values
+            {
+                if (BMI < 19)
+                {
+                    return "you are underweight";
+                }
+                if (BMI >= 19 & BMI <= 24)
+                {
+                    return "you are a normal weight";
+                }
+                if (BMI > 24)
+                {
+                    return "you are overweight";
+                }
+
+            } else //if user is male, display result based on these values
+            {
+                if (BMI < 20)
+                {
+                    return "you are underweight";
+                }
+                if (BMI >= 20 & BMI <= 25)
+                {
+                    return "you are a normal weight";
+                }
+                if (BMI > 25)
+                {
+                    return "you are overweight";
+                }
+            }
+            return null; //if user doesnt specify wether they are male or female, return null;
+
+        }
+
+
+    }
+}
